f942b7b2a7fed1e3775e2bff553fb43255422a0f
Paul <paulabes@users.noreply.github.com>
2025-07-10 18:59:21 +0100

    fix heruko

--- greatbritishbeer/settings.py
+++ greatbritishbeer/settings.py
@@ -27,10 +27,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-this-in-production')
+SECRET_KEY = config(
+    'SECRET_KEY',
+    default='43qgcy27_(sc(k!32j736or_4)v6i++0%cljul=f^=l-*dtl*w'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
